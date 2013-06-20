271c14b499fbdcf44ae00274f9b1b30c0ea3787b
dilgerm <martin@effectivetrainings.de>
2013-06-20 12:34:56 +0200

    Part 2 - Introducing WebResourceBundles

--- src/main/java/de/effectivetrainings/HomePage.java
+++ src/main/java/de/effectivetrainings/HomePage.java
@@ -5,6 +5,7 @@ import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.internal.HtmlHeaderContainer;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.request.resource.PackageResourceReference;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
@@ -17,5 +18,11 @@ public class HomePage extends WebPage {
     public void renderHead(HtmlHeaderContainer container) {
         super.renderHead(container);
         container.getHeaderResponse().render(JavaScriptHeaderItem.forReference(Bootstrap.responsive()));
+        container.getHeaderResponse().render(JavaScriptHeaderItem.forReference(new PackageResourceReference(HomePage.class,"file1.js")));
+        container.getHeaderResponse().render(JavaScriptHeaderItem.forReference(new PackageResourceReference(HomePage.class,"file2.js")));
+        container.getHeaderResponse().render(JavaScriptHeaderItem.forReference(new PackageResourceReference(HomePage.class,"file3.js")));
+        container.getHeaderResponse().render(JavaScriptHeaderItem.forReference(new PackageResourceReference(HomePage.class,"file4.js")));
+        container.getHeaderResponse().render(JavaScriptHeaderItem.forReference(new PackageResourceReference(HomePage.class,"file5.js")));
+        container.getHeaderResponse().render(JavaScriptHeaderItem.forReference(new PackageResourceReference(HomePage.class,"file6.js")));
     }
 }
--- src/main/java/de/effectivetrainings/WicketApplication.java
+++ src/main/java/de/effectivetrainings/WicketApplication.java
@@ -1,7 +1,12 @@
 package de.effectivetrainings;
 
+import org.apache.wicket.ajax.WicketEventJQueryResourceReference;
+import org.apache.wicket.bootstrap.Bootstrap;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.request.resource.JavaScriptPackageResource;
+import org.apache.wicket.request.resource.JavaScriptResourceReference;
+import org.apache.wicket.resource.JQueryResourceReference;
 
 /**
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
@@ -26,7 +31,17 @@ public class WicketApplication extends WebApplication
 	public void init()
 	{
 		super.init();
-
-		// add your configuration here
+        getResourceBundles().addCssBundle(WicketApplication.class,"app.css", Bootstrap.BOOTSTRAP_CSS, Bootstrap.BOOTSTRAP_RESPONSIVE_CSS);
+		getResourceBundles().addJavaScriptBundle(WicketApplication.class, "app.js",
+            JQueryResourceReference.get(),
+            WicketEventJQueryResourceReference.get(),
+            Bootstrap.responsive(),
+            new JavaScriptResourceReference(HomePage.class, "file1.js"),
+            new JavaScriptResourceReference(HomePage.class, "file2.js"),
+            new JavaScriptResourceReference(HomePage.class, "file3.js"),
+            new JavaScriptResourceReference(HomePage.class, "file4.js"),
+            new JavaScriptResourceReference(HomePage.class, "file5.js"),
+            new JavaScriptResourceReference(HomePage.class, "file6.js")
+        );
 	}
 }
