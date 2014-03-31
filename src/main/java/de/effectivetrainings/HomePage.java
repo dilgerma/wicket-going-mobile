cbcb6a0917af8b056a842e9ccdd40ba7c56e8b1a
dilgerm <martin@effectivetrainings.de>
2014-03-31 11:39:54 +0200

    Fix renderHead-Override
    Change-Id: Ic5ca93a0f26e872d7a72ad1d4a2f0b70a09f13bd

--- src/main/java/de/effectivetrainings/HomePage.java
+++ src/main/java/de/effectivetrainings/HomePage.java
@@ -2,9 +2,9 @@ package de.effectivetrainings;
 
 import org.apache.wicket.bootstrap.Bootstrap;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
-import org.apache.wicket.markup.head.filter.HeaderResponseContainer;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.internal.HtmlHeaderContainer;
+import org.apache.wicket.markup.head.IHeaderResponse; 
+import org.apache.wicket.markup.head.filter.HeaderResponseContainer;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.request.resource.PackageResourceReference;
 
@@ -17,14 +17,14 @@ public class HomePage extends WebPage {
     }
 
     @Override
-    public void renderHead(HtmlHeaderContainer container) {
-        super.renderHead(container);
-        container.getHeaderResponse().render(JavaScriptHeaderItem.forReference(Bootstrap.responsive()));
-        container.getHeaderResponse().render(JavaScriptHeaderItem.forReference(new PackageResourceReference(HomePage.class,"file1.js")));
-        container.getHeaderResponse().render(JavaScriptHeaderItem.forReference(new PackageResourceReference(HomePage.class,"file2.js")));
-        container.getHeaderResponse().render(JavaScriptHeaderItem.forReference(new PackageResourceReference(HomePage.class,"file3.js")));
-        container.getHeaderResponse().render(JavaScriptHeaderItem.forReference(new PackageResourceReference(HomePage.class,"file4.js")));
-        container.getHeaderResponse().render(JavaScriptHeaderItem.forReference(new PackageResourceReference(HomePage.class,"file5.js")));
-        container.getHeaderResponse().render(JavaScriptHeaderItem.forReference(new PackageResourceReference(HomePage.class,"file6.js")));
+    public void renderHead(IHeaderResponse response) {
+        super.renderHead(response);
+        response.render(JavaScriptHeaderItem.forReference(Bootstrap.responsive()));
+        response.render(JavaScriptHeaderItem.forReference(new PackageResourceReference(HomePage.class,"file1.js")));
+        response.render(JavaScriptHeaderItem.forReference(new PackageResourceReference(HomePage.class,"file2.js")));
+        response.render(JavaScriptHeaderItem.forReference(new PackageResourceReference(HomePage.class,"file3.js")));
+        response.render(JavaScriptHeaderItem.forReference(new PackageResourceReference(HomePage.class,"file4.js")));
+        response.render(JavaScriptHeaderItem.forReference(new PackageResourceReference(HomePage.class,"file5.js")));
+        response.render(JavaScriptHeaderItem.forReference(new PackageResourceReference(HomePage.class,"file6.js")));
     }
 }
